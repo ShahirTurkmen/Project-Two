22ca61dfc0dd3666a56fbccee9e9f5e60d640ce1
ShahirTukmen <shahirturkmen@gmail.com>
2024-02-23 13:37:56 +0430

    commit

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const modal = document.querySelector("#modal");
+const img12 = document.querySelectorAll("#img12");
+const closeButton = document.getElementById("closeButton");
+const overlay = document.querySelector("#overlay");
+const modal1 = document.querySelector("#modal1");
+const overlay1 = document.querySelector("#overlay1");
 const login = document.querySelector('.login');
+const img2 = document.querySelector('#img2');
+const taskbar = document.querySelector('.taskbar');
 const submit = document.querySelector('#submit');
+const webweb = document.querySelector('.webweb');
+const webweb1 = document.querySelector('.webweb1');
 const img = document.querySelector('#img');
 const inputPassword = document.querySelector('#password');
 const div = document.querySelector('#div');
@@ -10,15 +20,18 @@ const button3 = document.querySelector('#button3');
 const button4 = document.querySelector('#button4');
 const button5 = document.querySelector('#button5');
 const button6 = document.querySelector('#button6');
+taskbar.style.display = 'none';
+
+const Pic = ['./images/1.webp', './images/2.webp', './images/3.webp', './images/4.webp', './images/5.webp', './images/6.webp', './images/7.webp', './images/8.webp'];
 playButtonAudio();
 otherThings();
 function otherThings() {
     submit.addEventListener('click', () => {
-        if (inputPassword.value === '12345678901') {
+        if (inputPassword.value === '') {
             div.innerHTML = `<h1 style='color:green;' id='div1'>Ho!!! Ho!!! Correct Password😯😯🔥😺😺🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳🥳!!!</h1>`;
             login.append(div);
             setTimeout(() => {
-                login.remove()
+                login.remove();
                 audio('./utils/startup2.mp3');
             }, 2000);
         } else {
@@ -30,12 +43,46 @@ function otherThings() {
             login.appendChild(div);
         }
     });
+    document.addEventListener('contextmenu', function (e) {
+        setTimeout(() => {
+            for (let i = 0; i < Pic.length; i++) {
+                console.log(Pic[i]);
+                this.body.style.background = `url("${Pic[i]}") no-repeat`;
+            }
+            this.body.style.backgroundSize = 'cover';
+            taskbar.style.display = 'block';
+        }, 2000);
+        e.preventDefault();
+    });
+    img2.addEventListener('click', () => {
+        modal.classList.toggle("open");
+        // modal1.classList.add('open');
+        // modal1.classList.add('open')
+    });
+
+    webweb1.addEventListener('click', () => {
+        // overlay1.classList.add('open1');
+        // modal1.classList.add('open1');
+        modal1.classList.toggle('open1');
+        overlay1.classList.toggle('open1');
+
+    });
+    closeButton.addEventListener('click', () => {
+        overlay1.classList.toggle('open1');
+    });
+    img12.forEach((e) => {
+        e.addEventListener('click', function () {
+            document.body.style.background = `url("${e.src}") no-repeat`;
+            document.body.style.backgroundSize = 'cover'
+            console.log();
+        });
+    });
+
 }
 
 function audio(soundFile) {
     const audio = new Audio(soundFile);
     audio.play();
-    return audio.attributes.item(1).ownerElement.currentSrc = soundFile;
 }
 
 function playButtonAudio() {
@@ -67,3 +114,4 @@ function playButtonAudio() {
         audio('./utils/startup2.mp3');
     });
 }
+
